37f4d3cc55b0dc949dc46fb913e29d435dcd6f63
Devraj Gautam <88303092+ItsMeDevraj@users.noreply.github.com>
2023-06-28 12:31:19 +0000

    math and num in js done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,48 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8966;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//******************** Maths ***********************
+
+// console.log(Math);
+
+// const myNum = 2344566;
+// console.log(Math.log10(myNum));
+
+// const anotherNum = Math.log10(myNum);
+// console.log(anotherNum);
+
+// const roundNum = Math.round(anotherNum);
+// console.log(roundNum);
+
+// console.log(Math.round(Math.log10(myNum)+1));
+
+// Math.ceil();
+// Math.floor();
+// Math.min();
+// Math.max();
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max =  20;
+
+console.log(Math.floor());
+
+console.log(Math.floor(Math.random() * (max  - min + 1)) + min)
