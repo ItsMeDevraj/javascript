293aa8cb5668b7466ecd4b2b5e378fb0ad77f566
Devraj Gautam <88303092+ItsMeDevraj@users.noreply.github.com>
2023-06-28 07:13:07 +0000

    conversion operation in js done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringtNumber = String(someNumber);
-console.log(stringtNumber);
-console.log(typeof stringtNumber);
+// console.log(stringtNumber);
+// console.log(typeof stringtNumber);
+
+
+//****************** Operations ******************
+
+let value =  3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);\
+
+let  str1 = "hello";
+let str2 = " dev";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // right to left conversion
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //first "1"+2 = "12"+2 = "122"
+// console.log(1 + 2 +  "2"); //first 1+2 = 3 + "2" = "32"
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter  = 100;
+
+let b = gameCounter++;
+console.log("value of b: ", b);
+console.log(gameCounter);
+
+let a = ++gameCounter;
+console.log("value of a: ", a);
+console.log(gameCounter);
+
+
