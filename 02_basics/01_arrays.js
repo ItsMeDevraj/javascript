c9df6c032065a8dd145dd67202ed145c9abbb194
Devraj Gautam <88303092+ItsMeDevraj@users.noreply.github.com>
2023-07-01 13:32:12 +0000

    Array basics in js done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6); // insert element to end
+// myArr.push(7);
+// myArr.pop(); // Remove element from end
+
+// myArr.unshift(9);//insert element at start
+// myArr.shift(); //remove element from start
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+
+const newArr = myArr.join();
+
+// .join() Adds all the elements of an array into a string, separated by the specified separator string.
+
+// console.log(typeof myArr);
+// console.log(typeof newArr);
+
